e7ee653dd25b2b9590a53b1e01cc2430347eea0c
Kovas Palunas <kovas.palunas@gmail.com>
2020-01-29 22:19:32 -0800

    Added photos!

--- photos_calendar_syncer.py
+++ photos_calendar_syncer.py
@@ -3,12 +3,14 @@ import datetime
 import pprint
 import pickle
 import os.path
+from httplib2 import Http
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly',
+          'https://www.googleapis.com/auth/photoslibrary.readonly']
 
 
 def main():
@@ -27,6 +29,8 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
+            # Get credentials from
+            # https://developers.google.com/calendar/quickstart/python#step_1_turn_on_the
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
@@ -34,19 +38,31 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    service = build('calendar', 'v3', credentials=creds)
+    # pprint.pprint(creds.__dict__)
+    photo_service = build('photoslibrary', 'v1', credentials=creds)
+    cal_service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Photos API
+    results = photo_service.albums().list(
+        pageSize=10, fields="nextPageToken,albums(id,title)").execute()
+    items = results.get('albums', [])
+    if not items:
+        print('No albums found.')
+    else:
+        print('Albums:')
+        for item in items:
+            print('{0} ({1})'.format(item['title'].encode('utf8'), item['id']))
 
     # Call the Calendar API
     print('Getting the upcoming 10 events')
-    events_result = service.events().list(
+    events_result = cal_service.events().list(
         calendarId='mgf5ho48h2a65185npg79tuvqc@group.calendar.google.com',
         timeMin=(datetime.datetime.utcnow() - datetime.timedelta(days=7)
                  ).isoformat() + 'Z',  # 'Z' indicates UTC time
         maxResults=10, singleEvents=True,
         orderBy='startTime').execute()
     events = events_result.get('items', [])
-    # pprint.pprint(service.calendarList().list().execute())
-
+    # pprint.pprint(cal_service.calendarList().list().execute())
     if not events:
         print('No upcoming events found.')
     for event in events:
